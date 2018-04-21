6558500e33f1bb9c0cdf7f0febb390de910f020e
Prasahnt Kumar <kr.prashant94@gmail.com>
2018-04-21 12:30:25 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+setup(name='pyimageocr',
+      version='1.0',
+      description='Python machine learning ocr.',
+      url='https://github.com/Krprashant94/pyimageocr',
+      author='Prashant Kumar',
+      author_email='kr.prashant94@gmail.com',
+      license='MIT',
+      packages=['pyimageocr'],
+      zip_safe=False,
+      )
